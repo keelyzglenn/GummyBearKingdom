29b50301bb980083532eb6cde4dbc5bb06a69bda
Keely Glenn <glenj392@newschool.edu>
2017-04-21 09:43:37 -0700

    add update and delete functionality and views

--- src/GummyBearKingdom/Controllers/ProductsController.cs
+++ src/GummyBearKingdom/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using GummyBearKingdom.Models;
 
 
@@ -32,6 +33,34 @@ namespace GummyBearKingdom.Controllers
         public IActionResult Create(Product product)
         {
             db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(items => items.ProductId == id);
+            return View(thisProduct);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisProduct = db.Products.FirstOrDefault(products => products.ProductId == id);
+            db.Products.Remove(thisProduct);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
